a292e9f913ebfed9523a79dbac4f7535077e315d
Kumar <thanikanti.naga.dinesh@hp.com>
2020-10-15 15:15:02 +0530

    Section 8 Angular Basics

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IProduct } from './models/product';
+import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Skinet';
+
+  products: IProduct[];
+  pagination: IPagination;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe(
+      (response: any) => {
+        this.products = response.data;
+        this.pagination = response;
+      },
+      (error) => {
+          console.log(error);
+      }
+    );
+  }
 }
